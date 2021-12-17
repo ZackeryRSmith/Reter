e1686301d5e22cbf393f74007ac86aaffc6d136a
ZackeryRSmith <zackery.smith82307@gmail.com>
2021-12-17 18:02:53 -0500

    init commit

---
+++ modules/TeaCat/teacat.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+###########################################################################     
+#                                                                         #     
+#                             TeaCat (Super Cat!)                         #     
+#                                      ~ A pretifyed version of the cat   #     
+#                                                                         #     
+#  Copyright (c) 2020, Zackery .R. Smith <zackery.smith82307@gmail.com>.  #     
+#                                                                         #     
+#  This program is free software: you can redistribute it and/or modify   #     
+#  it under the terms of the GNU General Public License as published by   #     
+#  the Free Software Foundation, either version 3 of the License, or      #     
+#  (at your option) any later version.                                    #     
+#                                                                         #     
+#  This program is distributed in the hope that it will be useful,        #     
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of         #     
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #     
+#  GNU General Public License for more details.                           #     
+#                                                                         #     
+#  You should have received a copy of the GNU General Public License      #     
+#  along with this program. If not, see <http://www.gnu.org/licenses/>.   #     
+#                                                                         #     
+###########################################################################
+                        #  Proudly written in Vim  #
+                        #    Zackery .R. Smith     #
+                        # github.com/ZackeryRSmith #
+                        ############################
+
+
+
+__author__ = "Zackery Smith"
+__email__ = "zackery.smith82307@gmail.com"
+__copyright__ = "Copyright © 2020 Zackery Smith. All rights reserved."
+__license__ = "GNU GPL-3.0"
+__version_info__ = (0, 0, 1)
+__version__ = ".".join(map(str, __version_info__))
+
+import sys
+sys.path.append("../../")
+import argparse
+from reter.reter import (
+    indicator,
+    Cursor,
+    captureKey
+)
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Iterable,
+    Iterator,
+    List,
+    Match,
+    Optional,
+    Pattern,
+    Sequence,
+    Set,
+    TextIO,
+    Tuple,
+    Union,
+    cast,
+)
+
+
+########################################
+# TEACAT
+########################################
+
+
