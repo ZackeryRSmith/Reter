1c681d76ebcb6c005378597e449b06624b977abd
ZackeryRSmith <zackery.smith82307@gmail.com>
2021-12-17 08:56:55 -0500

    init commit

--- modules/bo-boxes/bo_boxes.py
+++ modules/bo-boxes/bo_boxes.py
@@ -1,3 +1,38 @@
+#!/usr/bin/env python
+###########################################################################     
+#                                                                         #     
+#                          Bo-Boxes (Combo-Boxes)                         #     
+#                                   ~ Creating combo boxes for the Unix   #
+#                                        shell has never been easier      #
+#                                                                         #     
+#  Copyright (c) 2020, Zackery .R. Smith <zackery.smith82307@gmail.com>.  #     
+#                                                                         #     
+#  This program is free software: you can redistribute it and/or modify   #     
+#  it under the terms of the GNU General Public License as published by   #     
+#  the Free Software Foundation, either version 3 of the License, or      #     
+#  (at your option) any later version.                                    #     
+#                                                                         #     
+#  This program is distributed in the hope that it will be useful,        #     
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of         #     
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #     
+#  GNU General Public License for more details.                           #     
+#                                                                         #     
+#  You should have received a copy of the GNU General Public License      #     
+#  along with this program. If not, see <http://www.gnu.org/licenses/>.   #     
+#                                                                         #     
+###########################################################################
+                        #  Proudly written in Vim  #
+                        #    Zackery .R. Smith     #
+                        # github.com/ZackeryRSmith #
+                        ############################
+
+## TODO ##
+# 1. Create multi-select box (selectionBox())
+# 2. Create a drop-down like box (dropdownBox())
+# 3. Create support for embedded choices (listBox()) 
+# 4. Simplify code, mainly adding some of these features into Reter
+
+
 __author__ = "Zackery Smith"
 __email__ = "zackery.smith82307@gmail.com"
 __copyright__ = "Copyright © 2020 Zackery Smith. All rights reserved."
@@ -41,6 +76,7 @@ def listBox(cursor, choices, theme: Optional[dict]={"pady": 0, "bullet": None, "
     :rtype: String
     :return: Returns selection 
     """
+    # Wow this looks complex, most of it is turnary operaters that allow you to create theming
 
     # Create "padx". It is a bit of a challenge to create so I left it out for now.
     
