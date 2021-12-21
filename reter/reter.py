30eb45c4012de935f600520967825bcdced3008e
ZackeryRSmith <zackery.smith82307@gmail.com>
2021-12-21 11:36:41 -0500

    Nothing new... messing around with features

--- reter/reter.py
+++ reter/reter.py
@@ -789,11 +789,14 @@ def main():
     terminal = start()
     print("The quick brown fox")
     print("Cool beans")
-    chunks = terminal.line.chunkIt(screen=terminal.screen, pattern=" ", lineNumber=1)
-    chunks[0].setColour(cursor=terminal.cursor, bg=indicator.colour.formatting.reverse)
-    chunks[1].setColour(cursor=terminal.cursor, fg=indicator.colour.fg.lightblue, bg=indicator.colour.formatting.reverse)
-    chunks[2].setColour(cursor=terminal.cursor, fg=indicator.colour.fg.magenta)
-    chunks[3].setColour(cursor=terminal.cursor, bg=indicator.colour.bg.red)
+    lineOne = terminal.line.chunkIt(screen=terminal.screen, pattern=" ", lineNumber=1)
+    lineOne[0].setColour(cursor=terminal.cursor, bg=indicator.colour.formatting.reverse)
+    lineOne[1].setColour(cursor=terminal.cursor, fg=indicator.colour.fg.lightblue, bg=indicator.colour.formatting.reverse)
+    lineOne[2].setColour(cursor=terminal.cursor, fg=indicator.colour.fg.magenta)
+    lineOne[3].setColour(cursor=terminal.cursor, bg=indicator.colour.bg.red)
+    lineTwo = terminal.line.chunkIt(screen=terminal.screen, pattern=" ", lineNumber=2)
+    lineTwo[0].setColour(cursor=terminal.cursor, bg=indicator.colour.bg.lightyellow)
+    lineTwo[1].setColour(cursor=terminal.cursor, fg=indicator.colour.formatting.underline, bg=indicator.colour.formatting.reverse)
 
 
 ########################################
