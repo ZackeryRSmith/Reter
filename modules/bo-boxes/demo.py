04b60926d035104d35fdce5f45e6458991573277
ZackeryRSmith <zackery.smith82307@gmail.com>
2021-12-24 08:55:25 -0500

    Demo works with buggy BoBoxes version.. not very modular though

--- modules/bo-boxes/demo.py
+++ modules/bo-boxes/demo.py
@@ -1,8 +1,9 @@
 import bo_boxes
 
-screen, line, cursor = bo_boxes.start()
-selection = bo_boxes.listBox(cursor, ["Stable", "Bleeding Edge", "Nightly"], theme={"pady": 0, "bullet": "○", "bulletSelection": "●", "bulletSpacing": " ", "selectionHighlight": None, "highlightBullet": False, "selectionTextColor": None, "textColor": None, "bulletColor": None, "bulletSelectionColor": None})
-#selection = bo_boxes.listBox(cursor, ["Stable", "Bleeding Edge", "Nightly"])
+terminal = bo_boxes.init()
+print("Select a version")
+#selection = bo_boxes.listBox(terminal, ["Stable", "Bleeding Edge", "Nightly"], theme={"pady": 0, "bullet": "○", "bulletSelection": "●", "bulletSpacing": " ", "selectionHighlight": None, "highlightBullet": False, "selectionTextColor": None, "textColor": None, "bulletColor": None, "bulletSelectionColor": None})
+selection = bo_boxes.listBox(terminal, ["Stable", "Bleeding Edge", "Nightly"])
 
 if selection == "Stable":
     print("Okay downloading stable version")
@@ -12,4 +13,3 @@ elif selection == "Bleeding Edge":
 
 elif selection == "Nightly":
     print("Okay downloading nightly version")
-    
