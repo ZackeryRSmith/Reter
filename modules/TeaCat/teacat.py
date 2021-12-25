4a6fc573e648bb36fa03231d9d850afc3a2cc5a5
ZackeryRSmith <zackery.smith82307@gmail.com>
2021-12-24 20:00:47 -0500

    Messing around with an idea

--- modules/TeaCat/teacat.py
+++ modules/TeaCat/teacat.py
@@ -34,12 +34,14 @@ __license__ = "GNU GPL-3.0"
 __version_info__ = (0, 0, 1)
 __version__ = ".".join(map(str, __version_info__))
 
+
+import os
 import sys
 sys.path.append("../../")
 import argparse
 from reter.reter import (
+    init,
     indicator,
-    Cursor,
     captureKey
 )
 from typing import (
@@ -65,4 +67,56 @@ from typing import (
 # TEACAT
 ########################################
 
+terminal = init()
+parser = argparse.ArgumentParser(description='TeaCat is a prettier version of the unix command cat. Out of the box it comes with script highlighting for python, javascript, rust, c, c++, c#, and sh! Along with line numbers, a file tree, and additonal file info. TeaCat also has plugin system so anyone can improve upon it!')
 
+# Create args
+parser.add_argument("Path", metavar='path', type=str,
+                    help="The path to file (Does work with relative paths!)")
+
+parser.add_argument("-s", action="store_true", default=True,
+                    dest="syntax_highlight",
+                    help="Syntax highlighting")
+
+parser.add_argument("-t", action="store_true", default=True,
+                    dest="file_tree",
+                    help="Display file tree")
+
+parser.add_argument("--noargs", action="store_true", default=False,
+                    dest="noargs",
+                    help="Disables all default args")
+
+# Parse args
+args = parser.parse_args()
+
+# Create argument variables
+passedPath = args.Path
+
+# Create sub variables
+fd = None  # Set later on 
+
+# Get initial pos
+initPos = terminal.cursor.getPos()
+
+# Check if given path is a folder or file
+if not os.path.isdir(passedPath):
+    if os.path.isfile(passedPath):
+        fd = "file"
+    else:
+        print('The path specified does not exist')
+        sys.exit()
+else:
+    fd = "folder"
+
+# IF file descripter is a folder
+if fd == "folder":
+    pass
+else:
+    fileContents = ""
+    # Read file
+    with open(passedPath, "r") as passedFile:
+        fileContents = passedFile.read()
+    # Print the number of lines the screen can hold
+    #sys.__stdout__.write(fileContents)
+    while True:  # Keep file running
+        pass
