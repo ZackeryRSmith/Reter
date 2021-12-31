0ac0b8be3b96d841ef1d7830753fcc8186b48a68
ZackeryRSmith <zackery.smith82307@gmail.com>
2021-12-30 23:42:05 -0500

    reter.py -> cursor

---
+++ reter/cursor/cursor.py
@@ -0,0 +1,148 @@
+########################################
+# CURSOR
+########################################
+
+class Cursor:
+    """
+    Terminal cursor.
+    """
+    def __init__(self, posx, posy, visibility: Optional[bool]=True):
+        """ 
+        Constructor to initialize the object
+        
+        :param int posx: Begining x position for cursor
+        :param int posy: Begining y position for cursor
+        :param bool visibility: True = Shown, False = Hidden
+        """
+        self.posx = posx
+        self.posy = posy
+        self.visibility = visibility
+        if self.visibility:
+            self.changeVisibility(True)
+        else:
+            self.changeVisibility(False)
+
+
+    def link(self):
+        """
+        Links cursor object to a screen object. This will auto restrict the cursor to the screen dimensions, if this is not what you are looking
+        for set `linkCursor=False` when creating a Screen object!
+        """
+        pass
+
+
+    def changeVisibility(self, visibility):
+        """
+        Changes the visibility of cursor
+
+        :param bool visibility: True = Shown, False = Hidden
+        """
+        self.visibility = visibility
+        if self.visibility:
+            sys.__stdout__.write("\x1b[?25h")
+        else:
+            sys.__stdout__.write("\x1b[?25l")
+
+
+    def getPos(self, xory: Optional[str]=None, updatePos: Optional[bool]=True):
+        """
+        Obtains position of cursor. This can be funky on some terminal emulators, for me my daily driver Terminator you must change up some
+        settings to get this code to work! This may be the same for your end-user. Make sure you keep this in mind while using getPos()!
+        
+        :param str xory: Choose what to return "x", or "y". Default None (Meaning it will return both x and y)
+        :param bool updatePos: Auto Updates cursor position after fetching row and col. Default is True
+
+        :rtype: tuple of int's
+        :return: Returns (column, row)
+        """
+        OldStdinMode = termios.tcgetattr(sys.stdin)
+        settings = termios.tcgetattr(sys.stdin)
+        settings[3] = settings[3] & ~(termios.ECHO | termios.ICANON)  # Disable echo, and stop the terminal from waiting for key press
+        termios.tcsetattr(sys.stdin, termios.TCSAFLUSH, settings)
+        try:
+            temp = ""
+            sys.__stdout__.write("\x1b[6n")  # Write mouse position silently
+            sys.__stdout__.flush()  # Allows the write() code above this line to be read
+            while not (temp := temp + sys.stdin.read(1)).endswith('R'):  # If you are confussed on this look at stackoverflow.com/questions/26000198/what-does-colon-equal-in-python-mean
+                pass
+            res = re.match(r".*\[(?P<y>\d*);(?P<x>\d*)R", temp)  # Creates groups for values using regex
+        finally:
+            termios.tcsetattr(sys.stdin, termios.TCSAFLUSH, OldStdinMode)  # Re-enables "echo"
+        if res:
+            if updatePos:
+                self.posx = res.group("x")
+                self.posy = res.group("y")
+            if xory == None:
+                return (int(res.group("x")), int(res.group("y")))  # Returns (x, y)
+            elif xory == "x":
+                return int(res.group("x"))
+            elif xory == "y":
+                return int(res.group("y"))
+            else:
+                raise IllegalArgumentError('"%s" is an illegal argument!' % (xory) + " Come on dude... it's in the variable name..")
+
+
+    def setPos(self, x, y):
+        """
+        Sets the position of cursor. If you are looking for changing the value and not setting the value look towards .move().
+
+        :param int x: x position to set
+        :param int y: y position to set
+        """
+        self.posx = int(x)
+        self.posy = int(y)
+        #-- Why do this you may as well it seemed like ESC[#;#H did not work. This did so I just opted for it
+        #   If you know the reason or can fix it make a pull request, create an issue under enhancement label
+        #   or shoot me an email!
+ 
+        # Get the distance between current postion, and the position to set
+        currentPos = self.getPos()
+        self.move(int(x)-int(currentPos[0]), int(y)-int(currentPos[1]))
+        #sys.__stdout__.write("\x1b[%s;%sH" % (self.posy, self.posx))
+
+
+    def returnPos(self):
+        """
+        """
+        return (self.posx, self.posy)
+
+
+    def move(self, x, y):
+        """
+        Changes the position of the cursor. If you are looking to set the value and not change the value look towards .setPos().
+
+        :param int x: x position to change
+        :param int y: y position to change
+        """
+        self.posx = x
+        self.posy = y
+        # Because of the way ESC[C works 0 must be ingnored.. Thus removing elif is not an option.
+        if self.posx > 0:  # +
+            sys.__stdout__.write('\x1b[%sC' % (self.posx))
+        elif self.posx < 0:  # -
+            sys.__stdout__.write('\x1b[%sD' % (int(str(self.posx)[1:])))  # Removes negative number with splicing
+        
+        if self.posy > 0:  # +
+            sys.__stdout__.write('\x1b[%sB' % (self.posy))
+        elif self.posy < 0:  # -
+            sys.__stdout__.write('\x1b[%sA' % (int(str(self.posy)[1:])))  # Removes negative number with splicing
+        sys.__stdout__.flush()  # Update line
+
+
+    def align(self, position):
+        """
+        Align cursor position with preset position E.g. "left" will put the cursor to the left side. "tright" will place the cursor in the
+        top right of the screen
+
+        :param string position: The position to go to I.e. 
+                                           "tleft" "tmiddle" "tright"
+                                            "left" "middle" "right"
+                                           "bleft" "bmiddle" "bright"
+        """
+        # Align still needs to be finished. With the knowledge and stuff implemented now this can be done right! I will get around to this...
+        # That is when I need the cursor.align function..
+
+        if position=="left":
+            self.move(-999, 0)
+        elif position=="right":
+            self.move(999, 0)
