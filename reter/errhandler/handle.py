fdcaa132f611db820a63b3fb4097b3c7f9938d7c
ZackeryRSmith <zackery.smith82307@gmail.com>
2021-12-31 10:36:27 -0500

    Main error handler

---
+++ reter/errhandler/handle.py
@@ -0,0 +1,34 @@
+# Requires style indicator
+########################################
+# ERRORS
+########################################
+
+class Error(Exception):
+    """Base class for other exceptions"""
+    def __init__(self, errorName, codeInQuestion, fixes: Optional[str]=None, info: Optional[str]=None):
+        self.errorName = errorName
+        self.codeInQuestion = codeInQuestion
+        self.fixes = fixes
+        self.info = info
+        print(FGRED+"Oops.. it seems an issue has occurred:\n\n"+EOC+FGRED+self.errorName+FGYELLOW+"\n+-----------------------------------+"+EOC)
+        print("""
+%sCode in question
+%s`
+%s
+`%s
+%s------------------------------------+""" % (FGYELLOW, FGCYAN, self.codeInQuestion, EOC, FGYELLOW))
+        if self.fixes != None:
+            print("""
+%sPotential fixes
+%s`
+%s
+`%s
+%s------------------------------------+""" % (FGYELLOW, FGCYAN, self.fixes, EOC, FGYELLOW))
+        if self.info != None:
+            print("""
+%sAdditional info
+%s`
+%s
+`%s""" % ((FGYELLOW, FGCYAN, self.info, EOC)))
+        print("%s+-----------------------------------+%s" % (FGYELLOW, EOC))    
+
