56bc30a0b73802cc9ba405a4048beb5bc85cb4c9
ZackeryRSmith <zackery.smith82307@gmail.com>
2021-12-31 10:37:08 -0500

    Called when an illegal argument is used

---
+++ reter/errhandler/errors/IllegalArgumentError.py
@@ -0,0 +1,8 @@
+# Requires handle.py
+########################################
+# ILLEGAL ARGUMENT ERROR
+########################################
+
+class IllegalArgumentError(Error):
+    """Called when a argument unbeknown to us gets passed"""
+    pass
