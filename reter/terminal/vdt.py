45e932a74833817354a34020b062ed51b693f2c4
ZackeryRSmith <zackery.smith82307@gmail.com>
2022-01-02 18:06:39 -0500

    Still learning and messing around with stdout

--- reter/terminal/vdt.py
+++ reter/terminal/vdt.py
@@ -1,1 +1,30 @@
+########################################
+# IMPORTS
+########################################
+
+import sys
+import os
+from contextlib import contextmanager 
+
+
+# https://stackoverflow.com/questions/4675728/redirect-stdout-to-a-file-in-python
+########################################
+# FILENO
+########################################
+
+def fileno(file_or_fd):
+    fd = getattr(file_or_fd, 'fileno', lambda: file_or_fd)()
+    if not isinstance(fd, int):
+        raise ValueError("Expected a file (`.fileno()`) or a file descriptor")
+    return fd
+
+
+########################################
+# STDOUT REDIRECT
+########################################
+
+def redirect():
+    
+
+sys.stdout = 
 
