f7251c72fa9adfd164f497b4b2c09efafee947eb
ZackeryRSmith <zackery.smith82307@gmail.com>
2022-01-03 12:29:56 -0500

    Adding some kind of error catching

--- src/lib/terminal/terminal.py
+++ src/lib/terminal/terminal.py
@@ -2,33 +2,34 @@ import os
 import sys
 import termios
 import tty
-# FPpaths is needed.. I will try to get around this
-#import fppaths
-#master = fppaths.get_master_path("src")
-#fppaths.set_abpath(master)
-
+from lib.terminal.screen import Screen, Line
+from lib.cursor.cursor import Cursor 
+from lib.errhandler.errors import UnexpectedResult
 
 
 class Terminal:
     def __init__(self):
         self.MEMORY = {}
-    
+
 
     def quick_start(self):
         """
-        Creates a Terminal object quickly, and in a unconfigurable manner. Good as long you don't need to over customize a TC object.
+        Creates a Terminal object quickly, and in a unconfigurable manner. Good as long you don't need to over customize a TC object. Will also validate tty.
 
         :rtype: object
         :return: Returns Terminal object with all TC objects pre-connected
         """
-        from lib.terminal.screen import Screen, Line
-        from lib.cursor.cursor import Cursor 
-        terminal = Terminal()
-        cursor = Cursor(0, 0)
-        screen = Screen(cursor)
-        line = Line(cursor)
-        terminal.connect(screen, line, cursor)
-        return self
+        if self.is_valid_tty():
+            terminal = Terminal()
+            cursor = Cursor(0, 0)
+            screen = Screen(cursor)
+            line = Line(cursor)
+            terminal.connect(screen, line, cursor)
+            return terminal
+        else:
+            raise ValueError("Cannot start a Terminal object. (It seems the output medium is not a valid terminal, are you using a terminal?)")
+            # Need to fix error system (And make it look better)
+            #raise UnexpectedResult.UnexpectedResult("UnexpectedResult", "if self.is_valid_tty():", "Use a terminal", "Cannot quick start a Terminal object. (It seems the output medium is not a valid terminal, are you using a terminal?)")
 
 
     def cache_tc_attr(self):
@@ -87,6 +88,8 @@ class Terminal:
         if self.is_valid_tty():  # Make sure tty is valid
             fd = sys.stdout.fileno()  # Get file descripter
             return termios.tcgetattr(fd)  # Get & return terminal attributes
+        else:
+            return "Failed to get tcattr. (It seems output medium is not a terminal, are you using a terminal?)"
 
 
     def connect(self, screen, line, cursor):
