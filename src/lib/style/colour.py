d744e8fcb28e12323802b7810a16a83c912926ab
ZackeryRSmith <zackery.smith82307@gmail.com>
2022-01-04 17:30:38 -0500

    Colour mappings, only 16 base colors as of now (256 ANSI support coming soon)

---
+++ src/lib/style/colour.py
@@ -0,0 +1,49 @@
+## Colours (16 basic fg, 16 basic bg)
+# Black
+FGBLACK        = "\u001b[30m"
+FGLIGHTBLACK   = "\u001b[30;1m"
+BGBLACK        = "\u001b[40m"
+BGLIGHTBLACK   = "\u001b[40;1m"
+# Red
+FGRED          = "\u001b[31m"
+FGLIGHTRED     = "\u001b[31;1m"
+BGRED          = "\u001b[41m"
+BGLIGHTRED     = "\u001b[41;1m"
+# Green
+FGGREEN        = "\u001b[32m"
+FGLIGHTGREEN   = "\u001b[32;1m"
+BGGREEN        = "\u001b[42m"
+BGLIGHTGREEN   = "\u001b[42;1m"
+# Yellow
+FGYELLOW       = "\u001b[33m"
+FGLIGHTYELLOW  = "\u001b[33;1m"
+BGYELLOW       = "\u001b[43m"
+BGLIGHTYELLOW  = "\u001b[43;1m"
+# Blue
+FGBLUE         = "\u001b[34m"
+FGLIGHTBLUE    = "\u001b[34;1m"
+BGBLUE         = "\u001b[44m"
+BGLIGHTBLUE    = "\u001b[44;1m"
+# Magenta
+FGMAGENTA      = "\u001b[35m"
+FGLIGHTMAGENTA = "\u001b[35;1m"
+BGMAGENTA      = "\u001b[45m"
+BGLIGHTMAGENTA = "\u001b[45;1m"
+# Cyan
+FGCYAN         = "\u001b[36m"
+FGLIGHTCYAN    = "\u001b[36;1m"
+BGCYAN         = "\u001b[46m"
+BGLIGHTCYAN    = "\u001b[46;1m"
+# White
+FGWHITE        = "\u001b[37m"
+FGLIGHTWHITE   = "\u001b[37;1m"
+BGWHITE        = "\u001b[47m"
+BGLIGHTWHITE   = "\u001b[47;1m"
+# Bold
+BOLD           = "\u001b[1m"
+# Underline
+UNDERLINE      = "\u001b[4m"
+# Reverse (Not text! Reverses the BG Colour!)
+REVERSED       = "\u001b[7m"                                
+# End of colour
+EOC            = "\u001b[0m"
