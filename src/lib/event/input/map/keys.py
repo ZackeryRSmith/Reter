16cf722025a90cefd40a90bf8fff11194872bedd
ZackeryRSmith <zackery.smith82307@gmail.com>
2022-01-04 18:42:39 -0500

    Key mappings, ANSI -> Python

---
+++ src/lib/event/input/map/keys.py
@@ -0,0 +1,38 @@
+### Define keys, ANSI -> Python
+# Number keys
+ONEKEY     = (b'1')
+TWOKEY     = (b'2')
+THREEKEY   = (b'3')
+FOURKEY    = (b'4')
+FIVEKEY    = (b'5')
+SIXKEY     = (b'6')
+SEVENKEY   = (b'7')
+EIGHTKEY   = (b'8')
+NINEKEY    = (b'9')
+ZEROKEY    = (b'0')
+
+# Function keys
+F1         = (b'^[OP')
+F2         = (b'^[OQ')
+F3         = (b'^[OR')
+F4         = (b'^[OS')
+F5         = (b'^[[15~')
+F6         = (b'^[[17~')
+F7         = (b'^[[18~')
+F8         = (b'^[[19~')
+F9         = (b'^[[20~')
+F10        = (b'^[[21~')
+F11        = (b'^[[23~')
+F12        = (b'^[[24~')
+
+UPARROW    = (b'\x1b[A')
+DOWNARROW  = (b'\x1b[B')
+RIGHTARROW = (b'\x1b[C')
+LEFTARROW  = (b'\x1b[D')
+BACKSPACE  = (b'\x7f')  # backspace/delete 
+RETURN     = (b'\r')  # return/enter
+FORMFEED   = (b'\x0c')  # ctrl+l
+XMIT       = (b'\x04')  # ctrl+d
+ETX        = (b'\x03')  # ctrl+c
+
+
