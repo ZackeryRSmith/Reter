5405a88fa7c24a28d7a938b0df47991e34603128
ZackeryRSmith <zackery.smith82307@gmail.com>
2022-01-05 18:26:11 -0500

    Added more keys

--- src/lib/event/input/map/keys.py
+++ src/lib/event/input/map/keys.py
@@ -1,4 +1,4 @@
-### Define keys, ANSI -> Python
+### Define keys, ANSI -> Python :: NOTE : Not 100% up-to-date with keymap dict
 # Number keys
 ONEKEY     = (b'1')
 TWOKEY     = (b'2')
@@ -38,12 +38,43 @@ ETX        = (b'\x03')  # ctrl+c
 
 # ANSI -> English
 keymap = {
+    #############################################################
+    #            Escape codes and names yoinked from,           #
+    #  https://www.physics.udel.edu/~watson/scen103/ascii.html  #
+    #-----------------------------------------------------------#
+    b'\x01':"soh",          # ctrl+a | Start of Heading
+    b'\x02':"stx",          # ctrl+b | Start of Text
+    b'\x03':"etx",          # ctrl+c | End of Text
+    b'\x04':"eot",          # ctrl+d | End of Xmit
+    b'\x05':"enq",          # ctrl+e | Enquiry
+    b'\x06':"ack",          # ctrl+f | Acknowledge
+    b'\x07':"bel",          # ctrl+g | Bell
+    b'\x08':"bs",           # ctrl+h | Backspace
+    b'\t':"ht",             # ctrl+i | Horizontal Tab
+    b'\n':"lf",             # ctrl+j | Line Feed
+    b'\x0b':"vt",           # ctrl+k | Vertical Tab
+    b'\x0c':"ff",           # ctrl+l | Form Feed
+    b'\r':"cr",             # ctrl+m | Carriage Feed
+    b'\x0e':"so",           # ctrl+n | Shift Out
+    b'\x0f':"si",           # ctrl+o | Shift In
+    b'\x10':"dle",          # ctrl+p | Data Line Escape
+    b'\x11':"dc1",          # ctrl+q | Device Control 1
+    b'\x12':"dc2",          # ctrl+r | Device Control 2
+    b'\x13':"dc3",          # ctrl+s | Device Control 3
+    b'\x14':"dc4",          # ctrl+t | Device Control 4
+    b'\x15':"nak",          # ctrl+u | Neg Acknowledge
+    b'\x16':"syn",          # ctrl+v | Synchronous Idel
+    b'\x17':"etb",          # ctrl+w | End of Xmit Block 	
+    b'\x18':"can",          # ctrl+x | Cancel
+    b'\x19':"em",           # ctrl+y | End of Medium
+    b'\x1a':"sub",          # ctrl+z | Substitute
+    b'\x1b':"esc",          # ctrl+[ | Escape
+    b'\x1c':"fs",           # ctrl+\ | File Separator
+    b'\x1d':"gs",           # ctrl+] | Group Separator
+    b'\x1e':"rs",           # ctrl+^ | Record Separator
+    b'\x1f':"us",           # ctrl+_ | Unit Separator
+    #-----------------------------------------------------------#
     b'\x1b[A':"uparrow", 
     b'\x1b[B':"downarrow", 
     b'\x1b[C':"rightarrow",
-    b'\x1b[D':"leftarrow",
-    b'\x7f':"backspace",
-    b'\r':"return",
-    b'\x0c':"formfeed",
-    b'\x04':"xmit",
-    b'\x03':"etx"}
+    b'\x1b[D':"leftarrow"}
