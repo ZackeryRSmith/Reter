acbfbe97f29b513a1f0da35597dcf7048aa946c1
ZackeryRSmith <zackery.smith82307@gmail.com>
2022-01-07 11:49:40 -0500

    Added stdout redirect

--- src/lib/terminal/term.py
+++ src/lib/terminal/term.py
@@ -6,7 +6,75 @@ from lib.terminal.screen import Screen, Line
 from lib.cursor.cursor import Cursor 
 from lib.event.input.keyboard.keyboard import Keyboard
 from lib.errhandler.errors import UnexpectedResult
-from typing import Optional
+from typing import (
+    Any,
+    AnyStr,
+    Callable,
+    Dict,
+    Iterable,
+    Iterator,
+    List,
+    Match,
+    Optional,
+    Pattern,
+    Sequence,
+    Set,
+    TextIO,
+    Tuple,
+    Union,
+    cast,
+)
+
+
+########################################
+# TERMINAL
+########################################
+class OutputRedirect(object):
+    def __init__(self):
+        self.name = sys.__stdout__.name  # Redirect name ("stdout" in most cases)
+        self.mode = sys.__stdout__.mode  # Current mode
+        self.errors = sys.__stdout__.errors  # Error type
+        self.encoding = sys.__stdout__.encoding  # stdout current encoding
+        self.closed = sys.__stdout__.closed  # Is closed?
+        self.buffer = sys.__stdout__.buffer  # Current buffer
+        self.line_buffering = sys.__stdout__.line_buffering  # Is line_buffering?
+        self.newlines = sys.__stdout__.newlines  # Is newlines?
+        self.cached_buffer = ""  # Cached screen variable
+
+    def write(self, s: AnyStr):
+        """
+        Just standard out but with a capturing system
+
+        :: IMPORTANT
+        : Make sure you add "\\n" to the end of your string, if you don't visual bugs will occur. 
+        : I am working on fixing this bug, and no it's not "simple" or else it would be fixed already.
+        : Feel free to give it a shot, open a pull request.
+        
+        :: RECOMENDATION
+        : Use print(), it passes through this function and adds a newline character for you. If you MUST use stdout.write() make sure you read
+        : the `IMPORTANT` section found above!
+        
+        :param str s: String to write
+        """
+        self.cached_buffer += s
+        sys.__stdout__.write(str(s))
+    
+    def writelines(self, lines: Iterable[AnyStr]):
+        for line in lines:
+            self.cached_buffer += line
+        sys.__stdout__.writelines(lines)
+
+    def writable(self) -> bool:
+        sys.__stdout__.writable()
+
+    def isatty(self) -> bool:
+        sys.__stdout__.isatty()
+
+    def fileno(self) -> int:
+        sys.__stdout__.fileno()
+
+    def flush(self):
+        sys.__stdout__.flush()
 
 
 ########################################
@@ -17,6 +85,8 @@ class Terminal:
     """Terminal manipulation object"""
     def __init__(self):
         """Constructor to initialize object"""
+        self.fd = sys.stdout.fileno()
+        self.saved_settings = termios.tcgetattr(self.fd)
         self.MEMORY = {}
 
     
@@ -33,33 +103,46 @@ class Terminal:
         """
         if self.is_valid_tty():
             self.create_alt_buffer()  # Create buffer
-            self.set_mode("cbreak")  # Make terminal readable
+            output = OutputRedirect()
+            self.redirect_stdout(output)
             cursor = Cursor(0, 0)
             screen = Screen(cursor, clear_screen=False)
             line = Line(cursor)
             keyboard = Keyboard(cursor)
-            self.connect(screen, line, cursor, keyboard)
+            self.connect(screen, line, cursor, keyboard, output)
             return self
         else:
             raise ValueError("Cannot start a Terminal object. (It seems the output medium is not a valid terminal, are you using a terminal?)")
 
 
     ###################
+    # Redirect stdout
+    ###################
+    def redirect_stdout(self, o: object):
+        """
+        Redirect stdout to an object, to go back to default pass in sys.__stdout__
+        """
+        sys.stdout = o
+
+
+    ###################
     # Set mode
     ###################
-    def set_mode(self, mode):
+    def set_mode(self, m: AnyStr):
         """
+        !:! NOT FINISHED !:!
+        
         Set terminal in 'raw', 'cooked', or 'cbreak' mode.
         
         :param str mode: The mode to set for terminal I.e. 'raw', 'cooked', or 'cbreak'
         """
-        if mode == "raw":
+        if m == "raw":
             pass
-        elif mode == "cooked":
+        elif m == "cooked":
             pass
-        elif mode == "cbreak":
+        elif m == "cbreak":
             self.fd = sys.stdin.fileno()
-            self.old_settings = termios.tcgetattr(self.fd)
+            self.saved_settings = termios.tcgetattr(self.fd)
             tty.setcbreak(self.fd)
         else:
             pass
@@ -173,20 +256,19 @@ class Terminal:
         Exits current screen buffer
         """
         sys.stdout.write("\033[?1049l")  # Restore screen
-        termios.tcsetattr(self.fd, termios.TCSADRAIN, self.old_settings)  # Restore old stdout settings
+        termios.tcsetattr(self.fd, termios.TCSADRAIN, self.saved_settings)  # Restore old stdout settings
 
 
     # Documented | txt | v0.1b
     ###################
     # Connect
     ###################
-    def connect(self, screen, line, cursor, keyboard: Optional[object]=None):
+    def connect(self, screen, line, cursor, keyboard: Optional[object]=None, output: Optional[object]=None):
         """
         Connects abstract terminal control (TC) objects into one object connected to a single terminal. 
         """
         self.screen, self.line, self.cursor = screen, line, cursor
         if keyboard != None:
             self.keyboard = keyboard
-
-
-
+        if output != None:
+            self.output = output
